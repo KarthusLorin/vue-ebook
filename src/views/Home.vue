26f651a29b3c206022dc8ad3dfa2f089a332172b
KarthusLorin <karthuslorin@gmail.com>
2018-12-01 20:30:11 +0800

    add icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="home">
-    <div id="read"></div>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
-  import Epub from 'epubjs'
-
-  global.ePub = Epub
   export default {
-    name: 'home',
-    mounted () {
-      this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
-      this.book.renderTo('read', {
-        width: window.innerWidth,
-        height: window.innerHeight
-      }).display()
-    }
   }
 </script>
