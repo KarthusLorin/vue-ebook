fcf49973f859f2fdfe873dec1abc22afa9b9dc0d
KarthusLorin <karthuslorin@gmail.com>
2018-12-01 21:02:40 +0800

    add web font

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
+    <span class="text">ABCDEFG</span>
     <router-view/>
   </div>
 </template>
 
 <style>
-
+  .text {
+    font-family: "Days One";
+    font-size: 20px;
+    color: orange;
+  }
 </style>
